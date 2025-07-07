859a5c8820ff2931b4a3dc885304fa5f3400ddd3
PolJaimejuanCaubet <poljaimejuan@HUAWEI-POLETE.localdomain>
2025-07-07 15:56:18 +0200

    refactor: refactor paths

--- src/index_documents.py
+++ src/index_documents.py
@@ -3,6 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
 import os
 
 def load_pdf(file_path):
@@ -27,22 +28,30 @@ def split_text_and_get_chunks(docs):
 
     return chunks
 
-def get_embedding_function():
+def get_embedding_function_Gemini():
+    load_dotenv()
     return GoogleGenerativeAIEmbeddings(
-        model="gemini-embedding-exp-03-07",
+        model="models/embedding-001",
         google_api_key = os.getenv('GOOGLE_API_KEY')
         )
 
+def get_embedding_function_HugginFace():
+    return HuggingFaceEmbeddings(model_name = "all-MiniLM-L6-v2")
+
 def create_and_save_vector_store(chunks): 
-    if not os.path.exists("faiss_index"):
-        vector_store = FAISS.from_texts(texts=chunks, embedding=get_embedding_function())
-        vector_store.save_local("faiss_index")
+    if not os.path.exists("../database/faiss_index"):
+        vector_store = FAISS.from_texts(texts=chunks, embedding=get_embedding_function_Gemini())
+        vector_store.save_local("../database/faiss_index")
     else:
-        vector_store = FAISS.load_local("faiss_index", get_embedding_function(),  allow_dangerous_deserialization=True)
+        vector_store = FAISS.load_local(
+            "../database/faiss_index",
+            get_embedding_function_Gemini(), 
+            allow_dangerous_deserialization=True
+            )
     
 
 def main():
-    file_path = '~/RAG_for_beginners/reglas_futsal_24-25.pdf'
+    file_path = '~/rag_basics/docs/reglas_futsal_24-25.pdf'
     docs = load_pdf(file_path)
     chunks = split_text_and_get_chunks(docs)
     create_and_save_vector_store(chunks)
